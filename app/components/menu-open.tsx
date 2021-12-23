0230d054c1bf846d440b3ef59d8ce3882cfba4f9
NonTechieGuides <rich@nontechieguides.io>
2021-12-23 16:19:56 -0600

    changes master to main

---
+++ app/components/menu-open.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function NavIcon() {
+  return (
+    <svg
+      className="h-6 w-6 fill-[#FFD553]"
+      viewBox="0 0 32 32"
+      xmlns="http://www.w3.org/2000/svg"
+      data-name="Layer 1"
+    >
+      <path d="m16 0a16 16 0 1 0 16 16 16 16 0 0 0 -16-16zm0 27a3 3 0 1 1 3-3 3 3 0 0 1 -3 3zm0-8a3 3 0 1 1 3-3 3 3 0 0 1 -3 3zm0-8a3 3 0 1 1 3-3 3 3 0 0 1 -3 3z" />
+      <circle cx="16" cy="8" r="1" />
+      <circle cx="16" cy="24" r="1" />
+      <circle cx="16" cy="16" r="1" />
+    </svg>
+  );
+}
+
+export default NavIcon;
